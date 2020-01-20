0d858568c7cea7fe1b736d132a75eb1a201d6c04
FrisodeJong <frisodejong1995@gmail.com>
2020-01-20 11:26:33 +0100

    Opmerkingen toegevoegd 204 and 357

--- 204_verdieping2.py
+++ 204_verdieping2.py
@@ -1,16 +1,20 @@
 import time
 import itertools
 
+# laat de gebruiker zijn gewenste type hamming nummer invoeren (input moet integer zijn)
 print("Voer het gewenste type hamming nummer in:")
 n = int(input())
 type_hamming = n
 
+# Het limiet is gelijk aan het kwadraat van het type
 limiet = type_hamming * type_hamming
-# print("Voer het gewenste limiet in:")
-# limiet = int(input())
 
+# Deze functie geeft de grootste priemfactor terug
 def grootste_priem_factor(n):
     i = 2
+    # Begin met 2 en start delen zolang het kan. 
+    # Zodra de priemfactor groter wordt dan gegeven type hamming nummer, 
+    # dan is het handig om meteen te stoppen (voor snelheid)
     while i * i <= n:
         if n % i == 0:
             n //= i
@@ -20,9 +24,12 @@ def grootste_priem_factor(n):
             break
     return n
 
+# Zet de starttijd om te kijken hoe lang het algoritme erover doet
 start_tijd = time.time()
 
+# Begin met 0 hamming nummers en tel er 1 bij op zodra er een nieuwe is gevonden
 teller = 0
+# Zet een counter aan die pas stopt op het moment als het limiet wordt overschreden
 for iterator in itertools.count(1,1):
     if iterator > limiet:
         break
@@ -30,9 +37,10 @@ for iterator in itertools.count(1,1):
     if priem_factor <= type_hamming:
         teller += 1
 
-# print("Gegeven type is", type_hamming)
-# print("Het gegeven limiet is", limiet)
+# Print de uitkomst samen met de gegevens
 print("Er zijn", teller, "hamming nummers van type", n, "onder de", limiet)
 
+# Stop de timer
 eind_tijd = time.time()
+# Print de tijd die het algoritme nodig had om het resultaat te vinden
 print(eind_tijd - start_tijd, "sec")
--- 357_verdieping2.py
+++ 357_verdieping2.py
@@ -2,6 +2,7 @@ import time
 import itertools
 
 # functie die checked of getal x een priemgetal is
+# In jouw geval is deze functie gegeven door is_prime(). Dus deze hieronder kan weg
 def is_priem(x):
     if x == 1:
         return False
@@ -12,33 +13,38 @@ def is_priem(x):
         i += 1
     return True
 
-# # lijst met priemgetallen
-# priem_lijst = (is_priem(x) for x in range(2, n))
-
-# functie die de delers van x geeft
+# Dit is een functie die de delers van input x geven. 
 def delers(x):
     for d in range(1,x):
         if x % d == 0:
             yield d
 
-# functie die voor een gegeven n kijkt of de formule een priemgetal geeft
+# Deze functie checked voor alle delers van n (gevonden door de functie delers(x)), 
+# of de deler + (n // deler) een priemgetal is.
 def priem_deler(n):
     for deler in delers(n):
         check = deler + (n // deler)
         if is_priem(check) == False:
             return False
 
+# Vraag de gebruiker voor het invoeren van een willekeurig integer getal.
 print("Voer een getal in:")
 n = int(input())
 
+# Zet de starttijd van de timer.
 start_tijd = time.time()
 
+# Start met tellen vanaf het gegeven getal en kijken voor elk getal of we 
+# de eigenschap hebben die bij de functie priem_deler(n) wordt gecheckt.
 for iterator in itertools.count(n+1,1):
     if priem_deler(iterator) == None:
         getal = iterator
         break
 
+# Print het resultaat.
 print("Het eerstvolgende getal met die bepaalde eigenschap is", getal)
 
+# Stop de timer.
 eind_tijd = time.time()
+# Print de tijd die het algoritme erover gedaan heeft.
 print(eind_tijd - start_tijd, "sec")
